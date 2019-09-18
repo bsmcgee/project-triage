7e2e7fec9bc34eba74a87722a84c5b1b5cb82481
bsmcgee <49524861+bsmcgee@users.noreply.github.com>
2019-09-18 11:12:23 -0400

    Class 9/18
    class

--- app/public/js/record.app.js
+++ app/public/js/record.app.js
@@ -1,13 +1,30 @@
 var patientRecordApp = new Vue({
   el: '#patientRecordApp',
   data: {
-    patients: []
+    patients: [],
+    formPatient: {
+      FistName: '',
+      LastName:'',
+      dob:'',
+      SexAtBirth: '',
+    }
   },
   methods: {
     fetchPatients() {
       fetch('dummy.php')
       .then(response => response.json())
       .then(json => { patientRecordApp.patients = json })
+    },
+    handleCreateRecord(event) {
+      //fetch(url, {method: 'past', data:thisFormPatient})
+      // .then()
+      this.patients.push(this.formPatient);
+      this.formPatient= {
+        FistName: '',
+        LastName:'',
+        dob:'',
+        SexAtBirth: '',
+      }
     }
   },
   created() {
